d831610dd3b6a90b16a5363e38675766a7e1273e
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-10-17 10:23:16 +0700

    mengubah status deleted true jika product dihapus

--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -177,12 +177,6 @@ func (ps *productService) DeleteProduct(ctx context.Context, request *product.De
 		return nil, err
 	}
 
-	imagePath := filepath.Join("storage", "product", productEntity.ImageFileName)
-	err = os.Remove(imagePath)
-	if err != nil {
-		return nil, err
-	}
-
 	return &product.DeleteProductResponse{
 		Base: utils.SuccessResponse("Delete product success"),
 	}, nil
